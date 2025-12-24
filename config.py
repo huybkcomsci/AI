10ffa97ab68e7b539d6c772b96935bff2fd9017f
Hus <hus@Huss-MacBook-Pro.local>
2025-12-24 18:40:35 +0700

    worked

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config:
     ENABLE_DEEPSEEK = bool(DEEPSEEK_API_KEY)
     
     # API timeout
-    REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT_SECONDS", "60"))
+    REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT_SECONDS", "600"))
     
     @classmethod
     def is_deepseek_available(cls):
